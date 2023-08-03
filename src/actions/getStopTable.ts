085a2b572de6e71d3f8320ffa9b2290a4419f868
Klimczewski <62210828+Dzban-Diego@users.noreply.github.com>
2023-08-03 16:17:31 +0200

    Wysyłanie tabeli odjazdów i przyjazdów

--- src/actions/getStopTable.ts
+++ src/actions/getStopTable.ts
@@ -1,22 +1,38 @@
 "use server";
 import { JSDOM } from "jsdom";
 
-async function getStopTable(lineId: string, stopID: string) {
+async function getStopTable(
+  lineId: string,
+  stopId: string
+): Promise<{ hour: string; departures: string[] }[]> {
   const html = await fetch(
-    `https://www.zditm.szczecin.pl/pl/pasazer/rozklady-jazdy/tabliczka/${lineId}/${stopID}/p/`
+    `https://www.zditm.szczecin.pl/pl/pasazer/rozklady-jazdy/tabliczka/${lineId}/${stopId}/p/75-dworzec-glowny`
   ).then((res) => {
     return res.text();
   });
   const dom = new JSDOM(html);
-  const mainElement = dom.window.document
-    .querySelector(".table-responsive")
-  const returnData = mainElement?.innerHTML
-    .replaceAll("[pokaż]", "")
-    .replaceAll("Godzina", "")
-    .replaceAll(/<td>\n.{1,}<\/td>/g, "<td class='nowrap'>--</td>")
-    .replaceAll(/href="/g, 'href="https://www.zditm.szczecin.pl/');
-  console.log(returnData)
-  return returnData || "";
+  const mainElement = dom.window.document.querySelector("main");
+  const table = mainElement?.querySelector(".table-responsive");
+  let data: { hour: string; departures: string[] }[] = [];
+
+  table?.querySelectorAll("th").forEach((th, index) => {
+    data[index] = { hour: th.innerHTML?.trim(), departures: [] };
+  });
+
+  table?.querySelectorAll("td").forEach((td, index) => {
+    const as = td.querySelectorAll("a");
+    as.forEach((a) => {
+      const spans = a.querySelectorAll("span");
+      spans.forEach((span) => {
+        if (span.innerHTML === "" || span.innerHTML.length > 5) {
+          return;
+        }
+        data[index].departures.push(span.innerHTML?.trim() || "");
+      });
+    });
+  });
+
+  return data || "";
 }
 
 export default getStopTable;
