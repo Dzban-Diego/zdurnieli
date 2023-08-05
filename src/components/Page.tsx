b91f647df2ed3e6c882ae57cf44e092bd89e6eea
Klimczewski <62210828+Dzban-Diego@users.noreply.github.com>
2023-08-05 21:23:02 +0200

    Cleanuw

--- src/components/Page.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-import React, { useCallback } from "react";
-import Head from "next/head";
-import Footer from "./Footer";
-import { RiMoonFill, RiMoonLine } from "react-icons/ri";
-
-export const Page: React.FC<PropsWithChildren> = ({ children }) => {
-  return null;
-};
