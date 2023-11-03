57fbc8ab00077ae2700b68ecb4ceb73283d66f27
Klimczewski <62210828+Dzban-Diego@users.noreply.github.com>
2023-11-03 18:22:12 +0100

    Dodanie lini z gdańska

--- src/actions/getLines.ts
+++ src/actions/getLines.ts
@@ -1,53 +1,66 @@
 "use server";
-import { parse } from 'node-html-parser';
 
-export default async function getLines() {
-  const html = await fetch(
-    "https://www.zditm.szczecin.pl/pl/pasazer/rozklady-jazdy,wedlug-linii"
-  ).then((res) => res.text());
-  const dom = parse(html);
-  const mainElement = dom.querySelector("main");
-  const categories: {
-    header: string;
-    lines: { name: string; id: string }[];
-  }[] = [];
-  const h2Elements = mainElement?.querySelectorAll("h2");
-  const hidden = [
-    "Ostatnie zmiany w rozkładach jazdy",
-    "Inne źródła rozkładów jazdy",
+import dayjs from "dayjs";
+
+type GD_lines = {
+  [key: string]: {
+    lastUpdate: string;
+    routes: {
+      routeId: number;
+      agencyId: number;
+      routeShortName: string;
+      routeLongName: string;
+      activationDate: string;
+      routeType: "BUS" | "TRAM" | "UNKNOWN";
+    }[];
+  };
+};
+
+type ReturnType = {
+  header: string;
+  lines: {
+    name: string;
+    id: string;
+  }[];
+}[];
+
+export default async function getLines(): Promise<ReturnType> {
+  const data = await fetch(
+    "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/22313c56-5acf-41c7-a5fd-dc5dc72b3851/download/routes.json"
+  ).then((res) => res.json() as Promise<GD_lines>);
+
+  // Pobranie dzisiejszych lini
+  const currentDate = dayjs().format("YYYY-MM-DD");
+  const routes = data[currentDate];
+  const types: ReturnType = [
+    { header: "Autobusy", lines: [] },
+    { header: "Tramwaje", lines: [] },
+    { header: "Autobusy nocne", lines: [] },
   ];
 
-  h2Elements?.forEach((element) => {
-    const header = element.innerHTML;
-    if (hidden.includes(header)) {
+  routes.routes.forEach((route) => {
+    if (route.routeType === "BUS" && route.routeShortName[0] !== "N") {
+      types[0].lines.push({
+        id: route.routeId.toString(),
+        name: route.routeShortName,
+      });
       return;
     }
-    categories.push({
-      header: header.trim(),
-      lines: [],
-    });
-  });
-  const ulElements = mainElement?.querySelectorAll(".row");
-  ulElements?.forEach((element, index) => {
-    const c = categories[index - 1];
-    const arr: { name: string; id: string }[] = [];
-
-    element.querySelectorAll(".visually-hidden").forEach((hidden) => {
-      hidden.remove();
-    });
-
-    element.querySelectorAll("a").forEach((a) => {
-      const urlArray = a.attrs.href.split("/");
-      arr.push({
-        name: a.text.trim(),
-        id: `${urlArray[urlArray.length - 2]}-${urlArray[urlArray.length - 1]}`,
+    if (route.routeType === "TRAM") {
+      types[1].lines.push({
+        id: route.routeId.toString(),
+        name: route.routeShortName,
       });
-    });
-
-    if (c) {
-      c.lines = arr;
+      return;
+    }
+    if (route.routeType === "BUS") {
+      types[2].lines.push({
+        id: route.routeId.toString(),
+        name: route.routeShortName,
+      });
+      return;
     }
   });
 
-  return categories;
+  return types;
 }
